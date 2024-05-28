df11851b00c9903d523dc5bd3ba4953bf2054549
Clément STOYANOV <c.stoyanov@it-students.fr>
2024-05-28 10:11:11 +0200

    ajout des fichiers du site

---
+++ js/slider-config.js
@@ -0,0 +1,13 @@
+    $(document).ready(function () {
+      $('#slider').lightSlider({
+        item: 1,
+        mode: 'fade',
+        pause:3200,
+        speed:2000,
+        pauseOnHover:true,
+        loop: true,
+        controls:false,
+        auto: true,
+        slideMargin: 0,
+      })
+    })
